7a00563af4fc2daecf892117e5e16ecca209fe5b
Celina <cbasa@qa.com>
2021-02-25 15:56:33 +0000

    Imported bootstrap stylesheet

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './resources/index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
